f9cff885df58d91a25f50c48525207e30920259a
EnisMollaahmed <enis_super02@outlook.com>
2025-03-29 17:14:50 +0200

    made header and navbar to not to dissappear when user scroll down

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,12 +2,15 @@ import Header from "../Components/Header/Header";
 import NavBar from "../Components/NavBar/NavBar";
 import NewsFeed from "../Components/NewsFeed/NewsFeed";
 
+
 export default function Home(){
     return (
         <>
             <Header/>
             <NavBar/>
-            <NewsFeed/>
+            <main className="content">
+                <NewsFeed/>
+            </main>
         </>
     )
 }
